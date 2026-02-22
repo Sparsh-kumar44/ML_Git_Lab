4c2d81ef455183de121ad95ab83491d47e6b928a
Sparsh Kumar <sparsh.kumar1412@gmail.com>
2026-02-22 23:57:41 +0530

    Added Scaling

--- model.py
+++ model.py
@@ -14,10 +14,12 @@ X = pd.get_dummies(X, drop_first=True)
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+scaler = StandardScaler()  
+X_train = scaler.fit_transform(X_train)  
+X_test = scaler.transform(X_test)
 
 model = LinearRegression()
 model.fit(X_train, y_train)
-
 
 y_pred = model.predict(X_test)
 
